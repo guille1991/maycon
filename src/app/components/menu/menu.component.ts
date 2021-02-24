01c79b51c766fc66339e4a671da6312f690dcb6c
Ponicla <jcponichan@gmail.com>
2021-02-23 23:44:22 -0300

    Arranque el buscador, muy suave

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 import { DataService } from '../../services/data.service';
-
+import { Producto } from './../../models/producto.model';
 
 @Component({
   selector: 'app-menu',
@@ -9,10 +10,21 @@ import { DataService } from '../../services/data.service';
 })
 export class MenuComponent implements OnInit {
 
+  productos_backup : Producto[];
   constructor(public dataService: DataService){
 
   }
   ngOnInit(): void {
+    this.productos_backup = this.dataService.productos;
   }
 
+  busca_producto(algo: any){
+    if (algo.keyCode == 13) {
+      Swal.fire({
+        icon: 'info',
+        html: 'Se intenta nuna búsqueda de '+ algo.target.value +' sobre el array? cuando vuelva a 0 recupera de un vector backup ?',
+        confirmButtonText: 'Dale'
+      })
+    }
+  }
 }
