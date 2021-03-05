7a2eaf61659f05c7623970c48190e9b74140bd36
Ponicla <jcponichan@gmail.com>
2021-03-04 22:10:17 -0300

    Init carrousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Maycon - Venta online';
 
+
+  constructor(public router: Router) {
+
+  }
+  
   onActivate(event: any) {
     window.scroll(0,0);
 }
